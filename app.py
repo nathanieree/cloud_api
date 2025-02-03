806100d762f3e0f2ef00157eafabd9444ee054d8
nathaniereee <namo.manzano.up@phinmaed.com>
2025-02-03 13:56:41 +0800

    push

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__, template_folder='template', static_folder='static')
 
-POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon?limit=50"
+POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon?limit=100"
+
+
 
 @app.route('/')
 def home():
@@ -15,7 +17,7 @@ def home():
         
         # Collect basic info (name, image, id)
         for item in data['results']:
-            pokemon_id = item['url'].split('/')[-2]
+            pokemon_id = item['url'].split('/')[-2] # Extract ID from URL
             pokemon_list.append({
                 "name": item['name'].capitalize(),
                 "image": f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_id}.png",
@@ -24,6 +26,21 @@ def home():
         
         return render_template('index.html', pokemon_list=pokemon_list)
     return "Error fetching Pokémon data."
+
+@app.route('/pokemon', methods=['GET'])
+def search_pokemon():
+    """Handle search form to look up Pokémon by name or ID."""
+    query = request.args.get('name', '').strip().lower()
+
+    if not query:
+        return redirect(url_for('home'))
+
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{query}")
+
+    if response.status_code == 200:
+        return redirect(url_for('pokemon_details', name=query))
+
+    return render_template('not_found.html', name=query), 404
 
 @app.route('/pokemon/<name>')
 def pokemon_details(name):
@@ -38,12 +55,12 @@ def pokemon_details(name):
             "height": data['height'],
             "weight": data['weight'],
             "abilities": [ability['ability']['name'] for ability in data['abilities']],
-            "types": [t['type']['name'] for t in data['types']]
+            "types": [t['type']['name'] for t in data['types']],
+            "stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]}
         }
         return render_template('pokemon.html', pokemon=pokemon_info)
-    else:
-        return render_template('not_found.html', name=name), 404
-
+    
+    return render_template('not_found.html', name=name), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
