7a707ded4c15b8e32ee5b3be75de697c4bc1a298
nathanieree <namo.manzano.up@phinmaed.com>
2025-03-21 23:50:19 +0800

    Fix: Set dynamic port for Render deployment

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+import os
 from flask import Flask
-from routes import home_bp, pokemon_bp, weather_bp, spotify_bp
+from routes import home_bp, pokemon_bp, weather_bp, spotify_bp, wikipedia_bp
 
-app = Flask(__name__, template_folder='template', static_folder='static')
-
-
-
-
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # Register Blueprints
 app.register_blueprint(home_bp)
 app.register_blueprint(pokemon_bp)
 app.register_blueprint(weather_bp)
 app.register_blueprint(spotify_bp)
+app.register_blueprint(wikipedia_bp)
 
+# Set PORT for Render deployment
+PORT = int(os.environ.get("PORT", 5000))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
